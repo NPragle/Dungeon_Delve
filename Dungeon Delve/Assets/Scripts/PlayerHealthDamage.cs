fbda07b50ee1c47e231c23d28944ec538f0e47a5
NPragle <npragle3@fingerlakes.edu>
2025-09-02 11:53:15 -0400

    enemy pt.2

---
+++ Dungeon Delve/Assets/Scripts/PlayerHealthDamage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthDamage : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("Player has died!");
+            // Add death handling logic here (e.g., respawn, game over screen)
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                health--;
+                Debug.Log("Player took damage! Health: " + health);
+            }
+        }
+}
--- Dungeon Delve/Assets/Scripts/PlayerMovement.cs
+++ Dungeon Delve/Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed = 5f;
     public float sprintSpeed = 10f;
 
-    public int maxHealth = 10;
-    public int health;
+    //private PlayerHealthDamage health;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        health = maxHealth;
+        //health = GetComponent<PlayerHealthDamage>();
     }
 
     // Update is called once per frame
@@ -26,21 +26,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(moveX, 0, moveY) * moveSpeed * Time.deltaTime;
         transform.Translate(movement, Space.World);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space)){
             Rigidbody rb = GetComponent<Rigidbody>();
-            if (rb != null && Mathf.Abs(rb.velocity.y) < 0.01f)
-            {
+            if (rb != null && Mathf.Abs(rb.velocity.y) < 0.01f){
                 rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
             }
         }
 
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
+        if (Input.GetKey(KeyCode.LeftShift)){
             moveSpeed = sprintSpeed;
         }
-        else
-        {
+        else{
             moveSpeed = walkSpeed;
         }
 
